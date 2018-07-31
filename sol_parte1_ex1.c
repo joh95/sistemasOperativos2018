0b6c3cfdf57053cb141f35ed35f73ca493ad500e
jdiegoisaza <39199649+jdiegoisaza@users.noreply.github.com>
2018-07-31 09:19:53 -0500

    Create sol_parte1_ex1.c
    Se termino el Primer ejercicio.

---
+++ sol_parte1_ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int venta,comision;
+  printf("Digite la venta: ");
+  scanf("%d", &venta);
+  if(venta <=20000){
+      comision= venta*0.05;
+      printf("Su comision fue de: %d\n", comision);
+      return comision;
+  }if(venta <=50000){
+      comision= venta*0.07+1000;
+      printf("Su comision fue de: %d\n", comision);
+      return comision;
+  }else{
+      comision= venta*0.1+3100;
+      printf("Su comision fue de: %d\n", comision);
+      return comision;
+  }
+  
+}
